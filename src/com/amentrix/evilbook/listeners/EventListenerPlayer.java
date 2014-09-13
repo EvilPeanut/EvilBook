5d6ec175edeb7868ca8bfb6e437971a956153d6e
EvilPeanut <buddypeanut@hotmail.co.uk>
2014-09-13 22:46:15 +0100

    Remove active potion effects

--- src/com/amentrix/evilbook/listeners/EventListenerPlayer.java
+++ src/com/amentrix/evilbook/listeners/EventListenerPlayer.java
@@ -708,6 +708,7 @@ public class EventListenerPlayer implements Listener {
 		if (EvilBook.isInSurvival(player) && !EvilBook.isInSurvival(event.getFrom())) {
 			EvilBook.setCreativeInventory(player);
 			EvilBook.getSurvivalInventory(player);
+			player.getActivePotionEffects().clear();
 			player.setGameMode(GameMode.SURVIVAL);
 			if (EvilBook.getProfile(player).isInvisible && EvilBook.getProfile(player).rank != Rank.SERVER_HOST) {
 				for (Player other : Bukkit.getServer().getOnlinePlayers()) other.showPlayer(player);
