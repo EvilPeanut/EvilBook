ff5c9de3f1b27753ac14c08f7c4e13183614e87b
Reece Lecrivain <reece@amentrix.com>
2016-08-08 19:20:44 +0100

    #69 Fix bug when leaving whilst disguised

--- src/com/amentrix/evilbook/main/Scheduler.java
+++ src/com/amentrix/evilbook/main/Scheduler.java
@@ -143,7 +143,7 @@ class Scheduler {
 			public void run() {
 				// Mob Disguise
 				for (PlayerProfile profile : EvilBook.playerProfiles.values()) {
-					if (profile.disguise != null) {
+					if (profile.disguise != null && profile.getPlayer() != null) {
 						//TODO: Throws exception causing console spam when player is leaving server whilst using disguise
 						profile.disguise.teleport(profile.getPlayer().getLocation());
 					}
